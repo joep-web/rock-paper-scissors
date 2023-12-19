176ffcaf50c66a1010c32408751b072ca97b2b80
joep-web <133377772+joep-web@users.noreply.github.com>
2023-12-19 15:46:53 +0000

    finished heh

--- script.js
+++ script.js
@@ -1,44 +1,90 @@
 //             CONSTANTS
 
-const humanChoice = document.querySelector(".Human div");
 
 const btn = document.querySelector("button");
 
 const CompChoice = document.querySelector(".Computer div");
 
+const humanChoice = document.querySelector(".Human div");
+
+const cRock = CompChoice.querySelector("#cRock");
+
+const cPaper = CompChoice.querySelector("#cPaper");
+
+const cScissors = CompChoice.querySelector("#cScissors");
+
+const cSpan = document.querySelector("#cSpan");
+
+const hSpan = document.querySelector("#hSpan");
+
+const roundsText = document.querySelector("#Rounds")
+  
 const choicesArr = [];
 
+let barrier = false;
+
+let rounds = 0;
+
+let hScore = 0;
+
+let cScore = 0;
+
+let comResult = "";
+
 //             LISTENERS
+
 
 humanChoice.addEventListener("click", (e) => {
   let choice = e.target;
   
-  if(/^h\w/.test(choice.id)) {
-    choice.style.opacity = "1";
-    choice.style.transform = "scale(1.1)";
-    choice.style.transition = "all .3s";
+  if(/^(R|P|S)\w/.test(choice.id)) {
+    scaleChoices(choice);
     choicesArr.push(choice.id);
-
+    CompChoice.style.borderColor = humanChoice.style.borderColor = "gray";
+    roundsText.innerText = `Rounds: ${rounds+=1}`;
+      
     for(let img of humanChoice.children) {
       if(img.id !== choice.id) {
-      img.style.opacity = "0.7";
-      img.style.transform = "scale(1.0)";
-      img.style.transition = "all .3s";
+      scaleDown(img);
+    }
+      
+    if(barrier === false){
+      createBarrier();
+      barrier = true;
     }
   }
 }});
 
 btn.addEventListener("click", () => {
+  CompChoice.querySelectorAll("img").forEach((img) => {
+    scaleDown(img);
+  });
+  
   if(choicesArr.length === 0) {
     chooseOptionWarning();
-  } else {
-    createBarrier();
+  }
+  if(barrier === true) {
+    barrierAnimationOut(CompChoice.querySelector("div"));
+    getComputerChoice();
   }
 });
 
 
 
 //           FUNCTIONS
+
+
+const scaleChoices = c => {
+  c.style.opacity = "1";
+  c.style.transform = "scale(1.1)";
+  c.style.transition = "all .3s";
+}
+
+const scaleDown = c => {
+  c.style.opacity = "0.7";
+  c.style.transform = "scale(1.0)";
+  c.style.transition = "all .3s";
+}
 
 const chooseOptionWarning = () => {
   const warning = document.createElement("div");
@@ -48,11 +94,18 @@ const chooseOptionWarning = () => {
   warning.className = "warning";
   document.body.appendChild(warning);
   warning.appendChild(txt);
-  barrierAnimations(warning);
+  barrierAnimationIn(warning);
+  barrierAnimationOut(warning);
 };
 
 const createBarrier = () => {
   const barrier = document.createElement("div");
+  createElementBarrier(barrier);
+  CompChoice.appendChild(barrier);
+  barrierAnimationIn(barrier);
+};
+
+const createElementBarrier = (barrier) => {
   barrier.style.transition = "all .3s";
   barrier.style.position = "absolute";
   barrier.style.width = "100%";
@@ -60,46 +113,86 @@ const createBarrier = () => {
   barrier.style.left = "0";
   barrier.style.transform = "translateY(-100%)";
   barrier.style.background = "#34495E";
-  CompChoice.appendChild(barrier);
-  barrierAnimations(barrier);
-};
+}
 
-const barrierAnimations = (b) => {
+const barrierAnimationIn = b => {
   setTimeout(() => {
     b.style.transform = "translateY(-13%)";
   });
+};
 
+const barrierAnimationOut = b => {
   setTimeout(() => {
     b.style.transform = "translateY(-100%)";
-  }, 1500);
+  }, 900);
   
   setTimeout(() => {
     b.parentElement.removeChild(b);
     choicesArr.length = 0;
+    barrier = false;
     for(let c of humanChoice.children) {
-      c.style.opacity = "0.7";
-      c.style.transform = "scale(1.0)";
-      c.style.transition = "all .3s";
+      scaleDown(c);
     }
-  }, 1800);
+  }, 1200);
 };
+
 
 const getComputerChoice = () => {
   const r = Math.floor(Math.random() * 3);
   if (r === 0){
-    console.log("Rock");
+    scaleChoices(cRock);
+    comResult = "Rock";
   } else if (r === 1){
-    console.log("Paper");
+    scaleChoices(cPaper);
+    comResult = "Paper";
   } else {
-    console.log("Scissors");
+    scaleChoices(cScissors);
+    comResult = "Scissors";
+  }
+
+  Winner(comResult);
+};
+
+const Winner = (r) => {
+  const winConditions = {
+    "Rock": "Scissors",
+    "Paper": "Rock",
+    "Scissors": "Paper"
+  };
+
+  const enemyChoice = r; // Assuming r is the enemy's choice
+  const yourChoice = choicesArr[choicesArr.length - 1];
+
+  if (enemyChoice === yourChoice) {
+    CompChoice.style.borderColor = humanChoice.style.borderColor = "blue";
+    CompChoice.style.transition = humanChoice.style.transition = "all .3s";
+    console.log("Tie");
+  } else if (winConditions[enemyChoice] === yourChoice) {
+    WinnerBorder(CompChoice, humanChoice)
+    console.log("Enemy Win");
+    cSpan.innerText = `Score: ${cScore+=1}`;
+  } else {
+    WinnerBorder(humanChoice, CompChoice);
+    console.log("You Win");
+    hSpan.innerText = `Score: ${hScore+=1}`;
   }
 };
 
-getComputerChoice();
+const WinnerBorder = (w, l) => {
+  w.style.borderColor = "green";
+  w.style.transition = "all .3s";
+  l.style.borderColor = "#800000";
+  l.style.transition = "all .3s";
+}
 
 
+
+  
 /*
 
+dapat may mapili muna yung player sa 3 choices pag wala hindi masstart game and may lalabas sa taas? na please select one ✓✓✓✓
+
+pag pinindot yung choice magiging opacity 1 tapos mag sscale using event listener then may lalabas na before/after na mag tatakip sa choice ng computer. ✓✓✓✓
 
 when the shoot is clicked matatawag yung function na mag rarandom sa choice ng computer tas ioopacity 1 then scale ang choice tapos tsaka mawawala yung takip
 
